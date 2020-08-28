3a66d5a11259d24cc0e2a9b635cc8eab9c6d6768
unknown <rmtestard@gmail.com>
2020-08-28 10:25:52 +0200

    I have implemented register request from client to server using urql

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
-import {
-  FormLabel,
-  FormControl,
-  FormErrorMessage,
-  Input,
-  Box,
-  Button,
-} from '@chakra-ui/core';
+import { Box, Button } from '@chakra-ui/core';
 import Wrapper from '../components/wrapper';
 import { InputField } from '../components/InputField';
+import { useMutation } from 'urql';
 
 interface registerProps {}
 
+const REGISTER_MUT = `mutation Register($username: String!, $password: String!){
+    register(options: { username: $username, password: $password}) {
+      errors {
+        field
+        message
+      }
+      user {
+        id
+        username
+      }
+    }
+  }
+  `;
+
 const Register: React.FC<registerProps> = ({}) => {
+  const [, register] = useMutation(REGISTER_MUT);
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: '', password: '' }}
         onSubmit={(values) => {
-          console.log(values);
+          return register(values);
         }}
       >
         {({ values, handleChange, isSubmitting }) => (
@@ -37,7 +46,12 @@ const Register: React.FC<registerProps> = ({}) => {
                 type="password"
               />
             </Box>
-            <Button mt={4} type="submit" variantColor="teal" isLoading={isSubmitting}>
+            <Button
+              mt={4}
+              type="submit"
+              variantColor="teal"
+              isLoading={isSubmitting}
+            >
               register
             </Button>
           </Form>
--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ import { UserResolver } from './resolvers/user';
 import redis from 'redis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
-import { MyContext } from './types';
+import cors from 'cors';
 
 const RedisStore = connectRedis(session);
 const redisClient = redis.createClient();
@@ -21,6 +21,13 @@ const main = async () => {
   orm.getMigrator().up();
 
   const app = express();
+
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    })
+  );
 
   app.use(
     session({
@@ -47,10 +54,10 @@ const main = async () => {
       validate: false,
     }),
     // accessible by all resolvers
-    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
+    context: ({ req, res }) => ({ em: orm.em, req, res }),
   });
 
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({ app, cors: false });
 
   app.listen(4000, () => {
     console.log('server started on localhost:4000');
