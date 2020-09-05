cd677da7605df0e4adb47ab8287d237b4a1b9917
unknown <rmtestard@gmail.com>
2020-09-05 10:30:10 +0200

    I have implemented logout on server and client

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -12,6 +12,7 @@ import { MyContext } from 'src/types';
 import { User } from '../entities/User';
 import argon2 from 'argon2';
 import { EntityManager } from '@mikro-orm/postgresql';
+import { COOKIE_NAME } from '../constants';
 
 @InputType()
 class UsernamePasswordInput {
@@ -153,5 +154,20 @@ export class UserResolver {
     return {
       user,
     };
+  }
+
+  @Mutation(() => Boolean)
+  logout(@Ctx() { req, res }: MyContext) {
+    return new Promise((resolve) =>
+      req.session.destroy((err) => {
+        if (err) {
+          console.log(err);
+          resolve(false);
+          return;
+        }
+        res.clearCookie(COOKIE_NAME);
+        resolve(true);
+      })
+    );
   }
 }
